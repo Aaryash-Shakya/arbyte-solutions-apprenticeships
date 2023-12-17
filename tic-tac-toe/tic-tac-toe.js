e53bb534b8a5ac339beb8b7aaae613fb17487c0c
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-12-17 22:14:24 +0545

    add: tic-tac-toe - handleGameEnd(), update score and reset board

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -20,17 +20,16 @@ const winningCombination = [
     [0, 4, 8],
     [2, 4, 6],
 ];
-const gameBox = document.getElementById('gameBox')
-const gameMessage = document.getElementById('gameMessage')
-const restartBtn = document.getElementById('restartBtn')
+const messageBox = document.getElementById("messageBox");
+const gameMessage = document.getElementById("gameMessage");
+const restartBtn = document.getElementById("restartBtn");
 // to handle turn and score
-const xScore = document.querySelector('#xScore')
-const oScore = document.querySelector('#oScore')
-const currentTurn = document.querySelector('#currentTurn')
+const xScore = document.querySelector("#xScore");
+const oScore = document.querySelector("#oScore");
+const currentTurn = document.querySelector("#currentTurn");
 
 let xTurn = 0;
 let moveCount = 0;
-
 
 // ! functions
 
@@ -67,18 +66,25 @@ const handleClick = cell => {
     cell.addEventListener(
         "click",
         () => {
-            moveCount++
+            // if cell already has mark, exit
+            if (cell.classList.contains("cross") || cell.classList.contains("circle")) {
+                console.log("cant click twice");
+                return;
+            }
+            moveCount++;
             cell.classList.add(xTurn ? "cross" : "circle");
 
             // check win
             let currentClass = xTurn ? "cross" : "circle";
             if (checkWin(currentClass)) {
                 console.log(`${currentClass} Wins`);
+                handleGameEnd(currentClass);
             }
 
             // check if board is full
-            else if (moveCount >= 9){
-                console.log('draw')
+            else if (moveCount >= 9) {
+                console.log("draw");
+                handleGameEnd("full");
             }
 
             // swap turn
@@ -103,12 +109,35 @@ const checkWin = currentClass => {
     });
 };
 
+// if someone wins display msg, increment score and reset
+const handleGameEnd = currentClass => {
+    messageBox.style.display = "flex";
+    if (currentClass === "cross") {
+        gameMessage.innerHTML = "X Wins";
+        // update score of x
+        xScore.innerText = Number(xScore.innerText) + 1;
+    }
+    if (currentClass === "circle") {
+        gameMessage.innerHTML = "O Wins";
+        // update score of o
+        oScore.innerText = Number(oScore.innerText) + 1;
+    } else if (currentClass === "full") {
+        gameMessage.innerHTML = "out of moves<br>DRAW";
+    }
+};
+
+// reset button is shown after game ends and msg is shown
+restartBtn.onclick = () => {
+    messageBox.style.display = "none";
+    startGame();
+};
+
 const startGame = () => {
     // reset the board by clearing all mark
     resetBoard();
 
     // reset moves
-    moveCount = 0
+    moveCount = 0;
 
     // add hover event listener
     cells.forEach(cell => handleHover(cell));
@@ -116,7 +145,6 @@ const startGame = () => {
     // add click event listener
     cells.forEach(cell => handleClick(cell));
 };
-
 
 // ! driver code
 
