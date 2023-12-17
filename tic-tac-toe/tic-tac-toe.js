18f224431a0346be90e142f62805df9254e36387
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-12-17 20:24:28 +0545

    add: tic-tac-toe - add mark on click
    make .circle and .corss have opacity 1 important
    add: handleClick() to add mark on click and swap turn
    rename: hoverEffect() to handleClick()

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -1,33 +1,46 @@
-const board = document.getElementById('board')
-const cells = document.querySelectorAll('.cell')
-let xTurn = 0
+const board = document.getElementById("board");
+const cells = document.querySelectorAll(".cell");
+let xTurn = 0;
 
 // clear all marks
 const clearMark = cell => {
-    cell.classList.remove('circle')
-    cell.classList.remove('hover-circle')
-    cell.classList.remove('cross')
-    cell.classList.remove('hover-cross')
-}
+    cell.classList.remove("circle");
+    cell.classList.remove("hover-circle");
+    cell.classList.remove("cross");
+    cell.classList.remove("hover-cross");
+};
 
 // show translucent mark on hover for each cell
-const hoverEffect = cell => {
+const handleHover = cell => {
     // when mouse enter a cell if x's turn then add .hover-cross, else add.hover-circle
-    cell.addEventListener('mouseenter',()=>{
-        cell.classList.add(xTurn?'hover-cross':'hover-circle')
-    })
+    cell.addEventListener("mouseenter", () => {
+        cell.classList.add(xTurn ? "hover-cross" : "hover-circle");
+    });
     // when mouse leave a cell if x's turn then remove .hover-cross, else remove.hover-circle
-    cell.addEventListener('mouseleave',()=>{
-        cell.classList.remove(xTurn?'hover-cross':'hover-circle')
-    })
-}
+    cell.addEventListener("mouseleave", () => {
+        cell.classList.remove(xTurn ? "hover-cross" : "hover-circle");
+    });
+};
+
+const handleClick = cell => {
+    // when mouse clicks a cell if x's turn then add .cross, else add .circle
+    cell.addEventListener("click", () => {
+        cell.classList.add(xTurn ? "cross" : "circle");
+
+        // swap turn
+        xTurn = xTurn ? false : true;
+    },{once:true});
+};
 
 const startGame = () => {
     // reset the board by clearing all mark
-    cells.forEach(cell => clearMark(cell))
+    cells.forEach(cell => clearMark(cell));
 
     // add hover event listener
-    cells.forEach(c=>hoverEffect(c))
-}
+    cells.forEach(cell => handleHover(cell));
 
-startGame()
+    // add click event listener
+    cells.forEach(cell=> handleClick(cell))
+};
+
+startGame();
