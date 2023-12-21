2d9f1b554e9fe2c8da17dc2046f761db849d64fc
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-12-21 12:10:21 +0545

    feat: ecommerce - add inc/dec quantity and delete item

--- ecommerce-clone/cart.js
+++ ecommerce-clone/cart.js
@@ -1,34 +1,39 @@
-let cartContainer = document.getElementById("cartContainer");
-let cartItems;
+// ! variables
 
-let index = 1;
-let cartString = "";
+let cartContainer = document.getElementById("cartContainer");
+let cartItems = [];
+
+// ! functions
 
 const fetchLocalStorage = () => {
     cartItems = JSON.parse(localStorage.getItem("cartItems"));
-    console.log(cartItems);
+};
+
+const updateLocalStorage = () => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
 };
 
 const updateCart = () => {
-    cartItems.forEach(async item => {
-        let data = fetch(`https://fakestoreapi.com/products/${item.id}`)
+    let cartString = "";
+    let index = 1;
+    cartItems.forEach(item => {
+        fetch(`https://fakestoreapi.com/products/${item.id}`)
             .then(res => res.json())
             .then(data => {
                 console.log(data);
                 cartString += `
                 <div class="cart-card border border-1 border-dark-subtle shadow-sm p-1 py-md-3 px-md-4 row">
-                    <p class="cart-number col-1">${index++}</p>
+                    <div class="cart-number col-1">${index++}</div>
                     <img src="${data.image}" class="cart-image col-2" width="40" height="40" alt="">
-                    <p class="cart-title col-4">${data.title}</p>
-                    <div class="cart-title col-3">
-                    <span class="btn btn-primary cart-subtract">-</span>
-                    <span class="cart-quantity">${item.quantity}</span>
-                    <span class="btn btn-primary cart-add">+</span>
+                    <div class="cart-title col-6">${data.title}</div>
+                    <div class="cart-title col-2">
+                        <span class="btn btn-primary cart-subtract" onclick="decreaseQuantity(${data.id})">-</span>
+                        <span class="cart-quantity">${item.quantity}</span>
+                        <span class="btn btn-primary cart-add" onclick="increaseQuantity(${data.id})">+</span>
                     </div>
-                    <p class="cart-delete col-1 btn btn-danger">Delete</p>
+                    <div class="cart-delete col-1 btn btn-danger" onclick="deleteItem(${data.id})">Delete</div>
                 </div>
                 `;
-                console.log(cartString);
                 cartContainer.innerHTML = cartString;
             })
             .catch(err => {
@@ -36,6 +41,46 @@ const updateCart = () => {
             });
     });
 };
+
+const deleteItem = id => {
+    let itemToDelete = cartItems.filter(item => item.id === id);
+    if (!confirm(`Are you sure you want to delete ${itemToDelete.title}`)) return;
+    cartItems = cartItems.filter(item => item.id !== id);
+    updateLocalStorage();
+    updateCart();
+};
+
+const increaseQuantity = id => {
+    cartItems = cartItems.map(item => {
+        if (item.id === id) {
+            // Create a new object with the updated quantity
+            return { ...item, quantity: item.quantity + 1 };
+        }
+        return item;
+    });
+
+    updateLocalStorage();
+    updateCart();
+};
+
+const decreaseQuantity = id => {
+    cartItems = cartItems.map(item => {
+        if (item.id === id) {
+            if (item.quantity <= 1) {
+                deleteItem(id);
+            } else {
+                return { ...item, quantity: item.quantity - 1 };
+            }
+            // Create a new object with the updated quantity
+        }
+        return item;
+    });
+
+    updateLocalStorage();
+    updateCart();
+};
+
+// ! driver code
 
 fetchLocalStorage();
 updateCart();
