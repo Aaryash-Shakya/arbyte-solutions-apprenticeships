256110d59f4f091510cfc95fd392b7635b89a87e
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-12-22 11:29:09 +0545

    add: reactjs25 complted accordian

--- 25-reactjs-projects/src/components/accordian/index.jsx
+++ 25-reactjs-projects/src/components/accordian/index.jsx
@@ -3,20 +3,32 @@ import data from './data'
 
 const Accordian = () => {
     const [selected, setSelected] = useState(null)
+
+    const handleSingleSelection = (id) => {
+        setSelected(id === selected ? null : id)
+    }
     return (
         <>
-            <div className="wrapper">
-                <div className="accordian w-100 h-100 d-flex align-items-center  ">
+            <div className="wrapper flex w-full h-fill justify-center items-center">
+                <div className="accordian w-6/12">
                     {
-                        data.map((item,index) => (
-                                <div className="item" key={index}>
-                                    <div className="title">
-                                        <h3>{item.question} hi</h3>
-                                        <span>+</span>
-                                    </div>
+                        data.map((item, index) => (
+                            <div className="item mb-3 py-3 px-5 bg-yellow-700 cursor-pointer" key={index} onClick={() => handleSingleSelection(item.id)}>
+                                <div className="title text-white flex justify-between items-center">
+                                    <h3>{item.question}</h3>
+                                    {
+                                        selected == item.id ? <span>-</span> : <span>+</span>
+                                    }
                                 </div>
-                            )
-                        )
+                                {
+                                    selected === item.id ?
+                                        <div className="content text-white h-auto">
+                                            {item.answer}
+                                        </div>
+                                        : null
+                                }
+                            </div>
+                        ))
                     }
                 </div>
             </div>
